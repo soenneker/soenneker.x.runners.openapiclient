b7f0d5101908176609931bf2a9706e152cd3bf4d
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-03-11 20:19:23 -0500

    organization, improves CI

--- src/Soenneker.X.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.X.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -58,15 +58,15 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
 
         await _processUtil.Start("dotnet", null, "tool update --global Microsoft.OpenApi.Kiota", waitForExit: true, cancellationToken: cancellationToken);
 
-        string srcDirectory = Path.Combine(gitDirectory, "src");
+        string srcDirectory = Path.Combine(gitDirectory, "src", Constants.Library);
 
         await DeleteAllExceptCsproj(srcDirectory, cancellationToken);
 
-        await _processUtil.Start("kiota", gitDirectory, $"kiota generate -l CSharp -d \"{filePath}\" -o src -c XOpenApiClient -n {Constants.Library} --ebc --cc",
+        await _processUtil.Start("kiota", gitDirectory, $"kiota generate -l CSharp -d \"{filePath}\" -o src/{Constants.Library} -c XOpenApiClient -n {Constants.Library} --ebc --cc",
                               waitForExit: true, cancellationToken: cancellationToken)
                           .NoSync();
 
-        string projFilePath = Path.Combine(gitDirectory, "src", $"{Constants.Library}.csproj");
+        string projFilePath = Path.Combine(gitDirectory, "src", Constants.Library, $"{Constants.Library}.csproj");
 
         await _dotnetUtil.Restore(projFilePath, cancellationToken: cancellationToken);
 
@@ -131,7 +131,7 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
 
     private async ValueTask BuildAndPush(string gitDirectory, CancellationToken cancellationToken)
     {
-        string projFilePath = Path.Combine(gitDirectory, "src", $"{Constants.Library}.csproj");
+        string projFilePath = Path.Combine(gitDirectory, "src", Constants.Library, $"{Constants.Library}.csproj");
 
         await _dotnetUtil.Restore(projFilePath, cancellationToken: cancellationToken);
 
